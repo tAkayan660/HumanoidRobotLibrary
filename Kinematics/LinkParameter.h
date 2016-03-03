972ee1a5e35524ed7ae8b47277dc750c7d3da55f
RyuYamamoto <ron_robotics@yahoo.co.jp>
2016-03-03 17:18:11 +0900

    add forward kinematics program

---
+++ Kinematics/LinkParameter.h
@@ -0,0 +1,58 @@
+/*
+ * @file		LinkParameter.h
+ * @brief		Constant Value of Link for Accelite
+ * @author	Ryu Yamamoto
+ * @date		2016/02/26
+ */
+
+#ifndef _LINK_PARAMETER_
+#define _LINK_PARAMETER_
+
+#include <iostream>
+#include <string>
+#include <Eigen/Dense>
+#include <limits>
+#include <boost/math/constants/constants.hpp>
+
+using namespace std;
+using namespace Eigen;
+
+static const double eps = numeric_limits<double>::epsilon();
+static const double pi = boost::math::constants::pi<double>();
+
+static const string joint_name[] = {
+	"WAIST",
+	"LINK_1",
+	"LINK_2",
+	"LINK_3"
+};
+
+enum
+{
+	WAIST=0,
+	LINK_1,
+	LINK_2,
+	LINK_3,
+	LEG_JOINT_NUM
+};
+
+static const int parent[LEG_JOINT_NUM] = {-1,0,1,2};
+static const int child[LEG_JOINT_NUM] = {1,2,3,-1};
+static const int sister[LEG_JOINT_NUM] = {-1,-1,-1,-1};
+
+static const double LinkAxis[LEG_JOINT_NUM][3] = {
+	{0,0,0},	//WASIT
+	{0,1,0},	//LINK_1
+	{0,1,0},	//LINK_2
+	{0,1,0}		//LINK_3
+};
+
+//For test
+static const double LinkPos[LEG_JOINT_NUM][3] = {
+	{0.0f, 0.0f, 0.0f},		//WAIST
+	{0.0f, 0.0f, 0.0f},	//LINK_1
+	{50.0f, 0.0f, 0.0f},	//LINK_2
+	{50.0f, 0.0f, 0.0f}	//LINK_3
+};
+
+#endif
---
+++ Kinematics/testKinematics.cpp
@@ -0,0 +1,19 @@
+#include "ForwardKinematics.h"
+
+using namespace std;
+using namespace MotionControl;
+
+int main(int argc, char* argv[])
+{
+	//Initialize Link info
+
+	//Consturctor of the ForwardKinematics Class
+	
+	//Calculation Forward Kinematics
+	
+	//Set Joint Angle
+	
+	//Calculation Forward Kinematics
+
+	return 0;
+}
