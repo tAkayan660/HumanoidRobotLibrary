111e0419f3873f67837855fb7b20388cdad88ac7
RyuYamamoto <ron_robotics@yahoo.co.jp>
2016-05-17 18:33:43 +0900

    Add a turning Operation(Incomplete)

--- src/GaitGenerator/LIP/LinearInvertedPendlum.cpp
+++ src/GaitGenerator/LIP/LinearInvertedPendlum.cpp
@@ -2,13 +2,19 @@
 
 using namespace cit;
 
+float deg2rad(float degree)
+{
+	float radian = M_PI * degree / 180.0f;
+	return radian; 
+}
+
 void LinearInvertedPendlum::SetFootStep()
 {
 	this->foot_step_list.push_back(Vector3f(0.0, 0.2, 0.0));
-	this->foot_step_list.push_back(Vector3f(0.3, 0.3, 0.0));
-	this->foot_step_list.push_back(Vector3f(0.3, 0.1, 0.0));
-	this->foot_step_list.push_back(Vector3f(0.3, 0.3, 0.0));
-	this->foot_step_list.push_back(Vector3f(0.0, 0.2, 0.0));
+	this->foot_step_list.push_back(Vector3f(0.25, 0.2, 20.0));
+	this->foot_step_list.push_back(Vector3f(0.25, 0.2, 40.0));
+	this->foot_step_list.push_back(Vector3f(0.25, 0.2, 60.0));
+	this->foot_step_list.push_back(Vector3f(0.0, 0.2, 60.0));
 	this->foot_step_list.push_back(Vector3f(0.0, 0.0, 0.0));
 	this->foot_step_list.push_back(Vector3f(0.0, 0.0, 0.0));
 }
@@ -28,7 +34,7 @@ void LinearInvertedPendlum::Integrate(int count)
 		y = (yi-pya)*cosh(t/Tc) + Tc*dyi*sinh(t/Tc) + pya;
 		dx = (xi-pxa)/Tc*sinh(t/Tc) + dxi*cosh(t/Tc);
 		dy = (yi-pya)/Tc*sinh(t/Tc) + dyi*cosh(t/Tc);
-		cog_list.push_back(Vector2f(x, y));
+		cog_list_x.push_back(x); cog_list_y.push_back(y);
 	}
 
 	//STEP4
@@ -40,18 +46,28 @@ void LinearInvertedPendlum::Integrate(int count)
 //STEP 5
 void LinearInvertedPendlum::CalcLegLandingPos()
 {
+#if 0
 	px = px + foot_step_list[count][0];
 	py = py - (pow(-1,count+1)*foot_step_list[count][1]);
-	p_list.push_back(Vector2f(px, py));
+#endif
+	px = p_list_x[count] + cos(deg2rad(foot_step_list[count][2]))*foot_step_list[count][0] - sin(deg2rad(foot_step_list[count][2]))*(-1*pow(-1,count+1)*foot_step_list[count][1]);
+	py = p_list_y[count] + sin(deg2rad(foot_step_list[count][2]))*foot_step_list[count][0] + cos(deg2rad(foot_step_list[count][2]))*(-1*pow(-1,count+1)*foot_step_list[count][1]);
+	p_list_x.push_back(px); p_list_y.push_back(py);
 }
 
 //STEP 6
 void LinearInvertedPendlum::CalcWalkFragment()
 {
-	xb = foot_step_list[count+1][0]/2;
-	yb = pow(-1,count+1)*foot_step_list[count+1][1]/2;
-	vxb = ((cosh(t_sup/Tc)+1)/(Tc*sinh(t_sup/Tc)))*xb;
-	vyb = ((cosh(t_sup/Tc)-1)/(Tc*sinh(t_sup/Tc)))*yb;
+#if 0
+	//xb = foot_step_list[count+1][0]/2;
+	//yb = pow(-1,count+1)*foot_step_list[count+1][1]/2;
+	//vxb = ((cosh(t_sup/Tc)+1)/(Tc*sinh(t_sup/Tc)))*xb;
+	//vyb = ((cosh(t_sup/Tc)-1)/(Tc*sinh(t_sup/Tc)))*yb;
+#endif
+	xb = cos(deg2rad(foot_step_list[count+1][2]))*foot_step_list[count+1][0]/2 - sin(deg2rad(foot_step_list[count+1][2]))*pow(-1,count+1)*foot_step_list[count+1][1]/2;
+	yb = sin(deg2rad(foot_step_list[count+1][2]))*foot_step_list[count+1][0]/2 + cos(deg2rad(foot_step_list[count+1][2]))*pow(-1,count+1)*foot_step_list[count+1][1]/2;
+	vxb = cos(deg2rad(foot_step_list[count+1][2]))*(1+C)/(Tc*S)*xb - sin(deg2rad(foot_step_list[count+1][2]))*(C-1)/(Tc*S)*yb;
+	vyb = sin(deg2rad(foot_step_list[count+1][2]))*(1+C)/(Tc*S)*xb + cos(deg2rad(foot_step_list[count+1][2]))*(C-1)/(Tc*S)*yb;
 }
 
 //STEP 7
@@ -66,8 +82,8 @@ void LinearInvertedPendlum::CalcGoalState()
 //STEP 8
 void LinearInvertedPendlum::ModifyLandPos()
 {
-	pxa = -a*(cosh(t_sup/Tc)-1)/D*(xd - cosh(t_sup/Tc)*xi - Tc*sinh(t_sup/Tc)*dxi) - b*sinh(t_sup/Tc)/(Tc*D)*(dxb - sinh(t_sup/Tc)/Tc*xi - cosh(t_sup/Tc)*dxi);
-	pya = -a*(cosh(t_sup/Tc)-1)/D*(yd - cosh(t_sup/Tc)*yi - Tc*sinh(t_sup/Tc)*dyi) - b*sinh(t_sup/Tc)/(Tc*D)*(dyb - sinh(t_sup/Tc)/Tc*yi - cosh(t_sup/Tc)*dyi);
-	p_modi_list.push_back(Vector2f(pxa, pya));
+	pxa = -a*(C-1)/D*(xd - C*xi - Tc*S*dxi) - b*S/(Tc*D)*(dxb - S/Tc*xi - C*dxi);
+	pya = -a*(C-1)/D*(yd - C*yi - Tc*S*dyi) - b*S/(Tc*D)*(dyb - S/Tc*yi - C*dyi);
+	p_modi_list_x.push_back(pxa); p_modi_list_y.push_back(pya);
 }
 
--- src/GaitGenerator/LIP/main.cpp
+++ src/GaitGenerator/LIP/main.cpp
@@ -1,13 +1,16 @@
 #include "LinearInvertedPendlum.h"
+#include "matplotlibcpp.h"
 
 using namespace cit;
+namespace plt = matplotlibcpp;
 
 int main(int argc, char* argv[])
 {
 	FILE *fp;
 	LinearInvertedPendlum LIP(0.8, 0.02, 10, 1);	//STEP 1
 	LIP.SetFootStep();	//STEP 2
-	LIP.p_list.push_back(Vector2f(0.0, 0.0));
+	LIP.p_list_x.push_back(0.0);
+	LIP.p_list_y.push_back(0.0);
 
 	/* Main Loop */
 	for(int n=0;n<6;n++)
@@ -17,14 +20,20 @@ int main(int argc, char* argv[])
 		LIP.CalcWalkFragment();		//STEP 6
 		LIP.CalcGoalState();		//STEP 7
 		LIP.ModifyLandPos();		//STEP 8
-		cout<<"px:"<<LIP.p_list[n][0]<<" py:"<<LIP.p_list[n][1]<<" pxa:"<<LIP.p_modi_list[n][0]<<" pya:"<<LIP.p_modi_list[n][1]<<endl;
+		//cout<<"px:"<<LIP.p_list_x[n]<<" py:"<<LIP.p_list_y[n]<<" pxa:"<<LIP.p_modi_list_x[n]<<" pya:"<<LIP.p_modi_list_y[n]<<endl;
 	}
 
+	plt::plot(LIP.cog_list_x, LIP.cog_list_y, "b");
+	plt::plot(LIP.p_list_x, LIP.p_list_y, ".g");
+	plt::plot(LIP.p_modi_list_x, LIP.p_modi_list_y, "+r");
+
+	plt::show();
+
 	/* Write Center of Mass to Csv File */
-	fp = fopen("InvetedPendlumCOG.csv", "w");
+	/*fp = fopen("InvetedPendlumCOG.csv", "w");
 	for(size_t i=0;i<LIP.cog_list.size();i++)
 		fprintf(fp,"%lf %lf\n",LIP.cog_list[i][0],LIP.cog_list[i][1]);
-	fclose(fp);
+	fclose(fp);*/
 
 	return 0;
 }
