5ed2af98e4fb8c389838983b841732b3bae21943
RyuYamamoto <ron_robotics@yahoo.co.jp>
2016-07-22 15:20:59 +0900

    changed directory

---
+++ src/kinematics/testKinematics.cpp
@@ -0,0 +1,29 @@
+#include "Kinematics.h"
+
+using namespace std;
+using namespace MotionControl;
+
+double rad2deg(double radian)
+{
+	double degree = radian * 180.0f / pi;
+	return degree;
+}
+
+double deg2rad(double degree)
+{
+	double radian = degree * pi / 180.0f;
+	return radian;
+}
+
+int main(int argc, char* argv[])
+{
+	//Initialize
+	
+	//Set Angle to Joint
+	
+	//Calculation Forward Kinematics
+	
+	//Calculation Inverse Kinematics
+	
+	return 0;
+}
