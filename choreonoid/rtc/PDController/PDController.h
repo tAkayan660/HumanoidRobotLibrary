471b6cf310a912b0de5d3aeb709fc4e2535f4968
Ryu Yamamoto <ron_robotics@yahoo.co.jp>
2016-12-14 08:36:48 +0900

    fixed PD Controller

--- choreonoid/rtc/PDController/PDController.h
+++ choreonoid/rtc/PDController/PDController.h
@@ -7,7 +7,6 @@
 #include <rtm/CorbaPort.h>
 #include <rtm/DataInPort.h>
 #include <rtm/DataOutPort.h>
-#include <cnoid/MultiValueSeq>
 #include <vector>
 
 class PDController : public RTC::DataFlowComponentBase
@@ -26,6 +25,9 @@ class PDController : public RTC::DataFlowComponentBase
 		// DataInPort declaration
 		RTC::TimedDoubleSeq m_angle;
 		RTC::InPort<RTC::TimedDoubleSeq> m_angleIn;
+		RTC::TimedDoubleSeq m_angleRef;
+		RTC::InPort<RTC::TimedDoubleSeq> m_angleRefIn;
+
 		// DataOutPort declaration
 		RTC::TimedDoubleSeq m_torque;
 		RTC::OutPort<RTC::TimedDoubleSeq> m_torqueOut;
