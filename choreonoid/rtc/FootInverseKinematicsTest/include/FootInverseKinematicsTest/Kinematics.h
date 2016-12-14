d855f614f94b81f893dee4245118d8500eedb80e
RyuYamamoto <ron_robotics@yahoo.co.jp>
2016-12-14 15:45:08 +0900

    enable test

---
+++ choreonoid/rtc/FootInverseKinematicsTest/include/FootInverseKinematicsTest/Kinematics.h
@@ -0,0 +1,36 @@
+/*
+ * @file		Kinematics.h
+ * @brief		Calculation Kinematics
+ * @author	Ryu Yamamoto
+ * @date		2016/03/02
+ */
+
+#ifndef _KINEMATICS_
+#define _KINEMATICS_
+
+#include "Link.h"
+
+namespace MotionControl
+{
+	class Kinematics
+	{
+	public:
+		struct Link *ulink;
+	public:
+		Kinematics(Link *ulink)
+		{
+			this->ulink = ulink;
+		}
+		void calcForwardKinematics(int rootlink);
+		MatrixXd calcJacobian(vector<int> idx); 
+		bool calcInverseKinematics(int to, Link target);
+		vector<int> FindRoute(int to);
+		Matrix<double,3,3> Rodrigues(Matrix<double,3,1> a, double q);
+		Matrix<double,3,1> rot2omega(Matrix<double,3,3> R);
+		Matrix<double,6,1> calcVWerr(Link Cref, Link Cnow);
+		template <typename t_matrix> t_matrix 
+		PseudoInverse(const t_matrix& m, const double &tolerance=1.e-6);
+	};
+};
+
+#endif
--- choreonoid/rtc/FootInverseKinematicsTest/src/FootInverseKinematicsTest.cpp
+++ choreonoid/rtc/FootInverseKinematicsTest/src/FootInverseKinematicsTest.cpp
@@ -12,20 +12,20 @@
 // Module specification
 // <rtc-template block="module_spec">
 static const char* footinversekinematicstest_spec[] =
-  {
-    "implementation_id", "FootInverseKinematicsTest",
-    "type_name",         "FootInverseKinematicsTest",
-    "description",       " Test program check Inverse Kinematics ",
-    "version",           "1.0.0",
-    "vendor",            "CIT",
-    "category",          "test",
-    "activity_type",     "PERIODIC",
-    "kind",              "DataFlowComponent",
-    "max_instance",      "1",
-    "language",          "C++",
-    "lang_type",         "compile",
-    ""
-  };
+{
+	"implementation_id", "FootInverseKinematicsTest",
+	"type_name",         "FootInverseKinematicsTest",
+	"description",       " Test program check Inverse Kinematics ",
+	"version",           "1.0.0",
+	"vendor",            "CIT",
+	"category",          "test",
+	"activity_type",     "PERIODIC",
+	"kind",              "DataFlowComponent",
+	"max_instance",      "1",
+	"language",          "C++",
+	"lang_type",         "compile",
+	""
+};
 // </rtc-template>
 
 /*!
@@ -33,12 +33,12 @@ static const char* footinversekinematicstest_spec[] =
  * @param manager Maneger Object
  */
 FootInverseKinematicsTest::FootInverseKinematicsTest(RTC::Manager* manager)
-    // <rtc-template block="initializer">
-  : RTC::DataFlowComponentBase(manager),
-    m_qCurIn("qCur", m_qCur),
-    m_qRefOut("qRef", m_qRef)
+	// <rtc-template block="initializer">
+	: RTC::DataFlowComponentBase(manager),
+	m_qCurIn("qCur", m_qCur),
+	m_qRefOut("qRef", m_qRef)
 
-    // </rtc-template>
+	  // </rtc-template>
 {
 }
 
@@ -53,115 +53,83 @@ FootInverseKinematicsTest::~FootInverseKinematicsTest()
 
 RTC::ReturnCode_t FootInverseKinematicsTest::onInitialize()
 {
-  // Registration: InPort/OutPort/Service
-  // <rtc-template block="registration">
-  // Set InPort buffers
-  addInPort("qCur", m_qCurIn);
-  
-  // Set OutPort buffer
-  addOutPort("qRef", m_qRefOut);
-  
-  // Set service provider to Ports
-  
-  // Set service consumers to Ports
-  
-  // Set CORBA Service Ports
-  
-  // </rtc-template>
+	addInPort("qCur", m_qCurIn);
 
-  // <rtc-template block="bind_config">
-  // </rtc-template>
-  
-  return RTC::RTC_OK;
+	addOutPort("qRef", m_qRefOut);
+
+	kine = new Kinematics(ulink);
+	SetJointInfo(ulink);
+
+	return RTC::RTC_OK;
 }
-
-/*
-RTC::ReturnCode_t FootInverseKinematicsTest::onFinalize()
-{
-  return RTC::RTC_OK;
-}
-*/
-
-/*
-RTC::ReturnCode_t FootInverseKinematicsTest::onStartup(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
-
-/*
-RTC::ReturnCode_t FootInverseKinematicsTest::onShutdown(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
-
 
 RTC::ReturnCode_t FootInverseKinematicsTest::onActivated(RTC::UniqueId ec_id)
 {
-  return RTC::RTC_OK;
-}
+	if(m_qCurIn.isNew()){
+		m_qCurIn.read();
+		m_qRef.data.length((m_qCur.data.length()));
+	}
+	
+	for(size_t i=0;i<JOINT_NUM;i++)
+		ulink[i+1].q = m_qCur.data[i];
+	for(size_t i=0;i<m_qCur.data.length();i++)
+		m_qRef.data[i] = m_qCur.data[i];
+	
+	kine->calcForwardKinematics(WAIST);
 
+	RFLink = ulink[RLEG_JOINT5];
+	LFLink = ulink[LLEG_JOINT5];
+
+	return RTC::RTC_OK;
+}
 
 RTC::ReturnCode_t FootInverseKinematicsTest::onDeactivated(RTC::UniqueId ec_id)
 {
-  return RTC::RTC_OK;
+	cout << "Deacvated" << endl;
+	return RTC::RTC_OK;
 }
 
 
 RTC::ReturnCode_t FootInverseKinematicsTest::onExecute(RTC::UniqueId ec_id)
 {
-  return RTC::RTC_OK;
-}
+	if(m_qCurIn.isNew()){
+		m_qCurIn.read();
+		
+		for(size_t i=0;i<JOINT_NUM;i++)
+			ulink[i+1].q = m_qCur.data[i];
 
-/*
-RTC::ReturnCode_t FootInverseKinematicsTest::onAborting(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
+		kine->calcForwardKinematics(WAIST);
 
-/*
-RTC::ReturnCode_t FootInverseKinematicsTest::onError(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
+		if(RFLink.p(2) < -650.0){
+			RFLink.p(2) += 0.1; LFLink.p(2) += 0.1;
 
-/*
-RTC::ReturnCode_t FootInverseKinematicsTest::onReset(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
+			if(kine->calcInverseKinematics(RLEG_JOINT5, RFLink) && kine->calcInverseKinematics(LLEG_JOINT5, LFLink)){
+				for(size_t i=0;i<12;i++)
+					m_qRef.data[i] = ulink[i+1].q;
+				cout << RFLink.p(2) << " " << LFLink.p(2) << endl;
+			}else{
+				cout << "Inverse Kinematics Faild." << endl;
+			}
+		}
+	}
+	m_qRefOut.write();
 
-/*
-RTC::ReturnCode_t FootInverseKinematicsTest::onStateUpdate(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
+	return RTC::RTC_OK;
 }
-*/
-
-/*
-RTC::ReturnCode_t FootInverseKinematicsTest::onRateChanged(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
 
 
 
 extern "C"
 {
- 
-  void FootInverseKinematicsTestInit(RTC::Manager* manager)
-  {
-    coil::Properties profile(footinversekinematicstest_spec);
-    manager->registerFactory(profile,
-                             RTC::Create<FootInverseKinematicsTest>,
-                             RTC::Delete<FootInverseKinematicsTest>);
-  }
-  
+
+	void FootInverseKinematicsTestInit(RTC::Manager* manager)
+	{
+		coil::Properties profile(footinversekinematicstest_spec);
+		manager->registerFactory(profile,
+				RTC::Create<FootInverseKinematicsTest>,
+				RTC::Delete<FootInverseKinematicsTest>);
+	}
+
 };
 
 
