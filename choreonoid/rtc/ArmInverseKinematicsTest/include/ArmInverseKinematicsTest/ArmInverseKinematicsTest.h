3cf160446fab059cab0b900f44c0d53bd5c3139c
RyuYamamoto <ron_robotics@yahoo.co.jp>
2016-12-16 15:59:39 +0900

    enable arm manipulation

--- choreonoid/rtc/ArmInverseKinematicsTest/include/ArmInverseKinematicsTest/ArmInverseKinematicsTest.h
+++ choreonoid/rtc/ArmInverseKinematicsTest/include/ArmInverseKinematicsTest/ArmInverseKinematicsTest.h
@@ -1,12 +1,3 @@
-// -*- C++ -*-
-/*!
- * @file  ArmInverseKinematicsTest.h
- * @brief Test code for arm control using inverse kinematics
- * @date  $Date$
- *
- * $Id$
- */
-
 #ifndef ARMINVERSEKINEMATICSTEST_H
 #define ARMINVERSEKINEMATICSTEST_H
 
@@ -14,264 +5,49 @@
 #include <rtm/idl/ExtendedDataTypesSkel.h>
 #include <rtm/idl/InterfaceDataTypesSkel.h>
 
-// Service implementation headers
-// <rtc-template block="service_impl_h">
-
-// </rtc-template>
-
-// Service Consumer stub headers
-// <rtc-template block="consumer_stub_h">
-
-// </rtc-template>
-
-// Service Consumer stub headers
-// <rtc-template block="port_stub_h">
-// </rtc-template>
-
-using namespace RTC;
-
 #include <rtm/Manager.h>
 #include <rtm/DataFlowComponentBase.h>
 #include <rtm/CorbaPort.h>
 #include <rtm/DataInPort.h>
 #include <rtm/DataOutPort.h>
 
-/*!
- * @class ArmInverseKinematicsTest
- * @brief Test code for arm control using inverse kinematics
- *
- */
+#include "Kinematics.h"
+
+using namespace MotionControl;
+using namespace RTC;
+
 class ArmInverseKinematicsTest
-  : public RTC::DataFlowComponentBase
+: public RTC::DataFlowComponentBase
 {
- public:
-  /*!
-   * @brief constructor
-   * @param manager Maneger Object
-   */
-  ArmInverseKinematicsTest(RTC::Manager* manager);
+	public:
 
-  /*!
-   * @brief destructor
-   */
-  ~ArmInverseKinematicsTest();
+		ArmInverseKinematicsTest(RTC::Manager* manager);
+		~ArmInverseKinematicsTest();
+		virtual RTC::ReturnCode_t onInitialize();
+		virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
+		virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
+		virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
+	protected:
+		RTC::TimedDoubleSeq m_qCur;
+		InPort<RTC::TimedDoubleSeq> m_qCurIn;
+		RTC::TimedFloatSeq m_axes;
+		InPort<RTC::TimedFloatSeq> m_axesIn;
+		RTC::TimedBooleanSeq m_button;
+		InPort<RTC::TimedBooleanSeq> m_buttonIn;
 
-  // <rtc-template block="public_attribute">
-  
-  // </rtc-template>
-
-  // <rtc-template block="public_operation">
-  
-  // </rtc-template>
-
-  /***
-   *
-   * The initialize action (on CREATED->ALIVE transition)
-   * formaer rtc_init_entry() 
-   *
-   * @return RTC::ReturnCode_t
-   * 
-   * 
-   */
-   virtual RTC::ReturnCode_t onInitialize();
-
-  /***
-   *
-   * The finalize action (on ALIVE->END transition)
-   * formaer rtc_exiting_entry()
-   *
-   * @return RTC::ReturnCode_t
-   * 
-   * 
-   */
-  // virtual RTC::ReturnCode_t onFinalize();
-
-  /***
-   *
-   * The startup action when ExecutionContext startup
-   * former rtc_starting_entry()
-   *
-   * @param ec_id target ExecutionContext Id
-   *
-   * @return RTC::ReturnCode_t
-   * 
-   * 
-   */
-  // virtual RTC::ReturnCode_t onStartup(RTC::UniqueId ec_id);
-
-  /***
-   *
-   * The shutdown action when ExecutionContext stop
-   * former rtc_stopping_entry()
-   *
-   * @param ec_id target ExecutionContext Id
-   *
-   * @return RTC::ReturnCode_t
-   * 
-   * 
-   */
-  // virtual RTC::ReturnCode_t onShutdown(RTC::UniqueId ec_id);
-
-  /***
-   *
-   * The activated action (Active state entry action)
-   * former rtc_active_entry()
-   *
-   * @param ec_id target ExecutionContext Id
-   *
-   * @return RTC::ReturnCode_t
-   * 
-   * 
-   */
-   virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
-
-  /***
-   *
-   * The deactivated action (Active state exit action)
-   * former rtc_active_exit()
-   *
-   * @param ec_id target ExecutionContext Id
-   *
-   * @return RTC::ReturnCode_t
-   * 
-   * 
-   */
-   virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
-
-  /***
-   *
-   * The execution action that is invoked periodically
-   * former rtc_active_do()
-   *
-   * @param ec_id target ExecutionContext Id
-   *
-   * @return RTC::ReturnCode_t
-   * 
-   * 
-   */
-   virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
-
-  /***
-   *
-   * The aborting action when main logic error occurred.
-   * former rtc_aborting_entry()
-   *
-   * @param ec_id target ExecutionContext Id
-   *
-   * @return RTC::ReturnCode_t
-   * 
-   * 
-   */
-  // virtual RTC::ReturnCode_t onAborting(RTC::UniqueId ec_id);
-
-  /***
-   *
-   * The error action in ERROR state
-   * former rtc_error_do()
-   *
-   * @param ec_id target ExecutionContext Id
-   *
-   * @return RTC::ReturnCode_t
-   * 
-   * 
-   */
-  // virtual RTC::ReturnCode_t onError(RTC::UniqueId ec_id);
-
-  /***
-   *
-   * The reset action that is invoked resetting
-   * This is same but different the former rtc_init_entry()
-   *
-   * @param ec_id target ExecutionContext Id
-   *
-   * @return RTC::ReturnCode_t
-   * 
-   * 
-   */
-  // virtual RTC::ReturnCode_t onReset(RTC::UniqueId ec_id);
-  
-  /***
-   *
-   * The state update action that is invoked after onExecute() action
-   * no corresponding operation exists in OpenRTm-aist-0.2.0
-   *
-   * @param ec_id target ExecutionContext Id
-   *
-   * @return RTC::ReturnCode_t
-   * 
-   * 
-   */
-  // virtual RTC::ReturnCode_t onStateUpdate(RTC::UniqueId ec_id);
-
-  /***
-   *
-   * The action that is invoked when execution context's rate is changed
-   * no corresponding operation exists in OpenRTm-aist-0.2.0
-   *
-   * @param ec_id target ExecutionContext Id
-   *
-   * @return RTC::ReturnCode_t
-   * 
-   * 
-   */
-  // virtual RTC::ReturnCode_t onRateChanged(RTC::UniqueId ec_id);
-
-
- protected:
-  // <rtc-template block="protected_attribute">
-  
-  // </rtc-template>
-
-  // <rtc-template block="protected_operation">
-  
-  // </rtc-template>
-
-  // Configuration variable declaration
-  // <rtc-template block="config_declare">
-
-  // </rtc-template>
-
-  // DataInPort declaration
-  // <rtc-template block="inport_declare">
-  
-  // </rtc-template>
-
-
-  // DataOutPort declaration
-  // <rtc-template block="outport_declare">
-  
-  // </rtc-template>
-
-  // CORBA Port declaration
-  // <rtc-template block="corbaport_declare">
-  
-  // </rtc-template>
-
-  // Service declaration
-  // <rtc-template block="service_declare">
-  
-  // </rtc-template>
-
-  // Consumer declaration
-  // <rtc-template block="consumer_declare">
-  
-  // </rtc-template>
-
- private:
-  // <rtc-template block="private_attribute">
-  
-  // </rtc-template>
-
-  // <rtc-template block="private_operation">
-  
-  // </rtc-template>
-
+		RTC::TimedDoubleSeq m_qRef;
+		OutPort<RTC::TimedDoubleSeq> m_qRefOut;
+	private:
+		Link ulink[JOINT_NUM];
+		Link RARM_Link;
+		Kinematics *arm;
+		float gain;
 };
 
 
 extern "C"
 {
-  DLL_EXPORT void ArmInverseKinematicsTestInit(RTC::Manager* manager);
+	DLL_EXPORT void ArmInverseKinematicsTestInit(RTC::Manager* manager);
 };
 
 #endif // ARMINVERSEKINEMATICSTEST_H
--- choreonoid/rtc/ArmInverseKinematicsTest/src/ArmInverseKinematicsTest.cpp
+++ choreonoid/rtc/ArmInverseKinematicsTest/src/ArmInverseKinematicsTest.cpp
@@ -1,48 +1,32 @@
-// -*- C++ -*-
-/*!
- * @file  ArmInverseKinematicsTest.cpp
- * @brief Test code for arm control using inverse kinematics
- * @date $Date$
- *
- * $Id$
- */
-
 #include "ArmInverseKinematicsTest.h"
 
-// Module specification
-// <rtc-template block="module_spec">
 static const char* arminversekinematicstest_spec[] =
-  {
-    "implementation_id", "ArmInverseKinematicsTest",
-    "type_name",         "ArmInverseKinematicsTest",
-    "description",       "Test code for arm control using inverse kinematics",
-    "version",           "1.0.0",
-    "vendor",            "CIT",
-    "category",          "test",
-    "activity_type",     "PERIODIC",
-    "kind",              "DataFlowComponent",
-    "max_instance",      "1",
-    "language",          "C++",
-    "lang_type",         "compile",
-    ""
-  };
-// </rtc-template>
+{
+	"implementation_id", "ArmInverseKinematicsTest",
+	"type_name",         "ArmInverseKinematicsTest",
+	"description",       "Test code for arm control using inverse kinematics",
+	"version",           "1.0.0",
+	"vendor",            "CIT",
+	"category",          "test",
+	"activity_type",     "PERIODIC",
+	"kind",              "DataFlowComponent",
+	"max_instance",      "1",
+	"language",          "C++",
+	"lang_type",         "compile",
+	""
+};
 
-/*!
- * @brief constructor
- * @param manager Maneger Object
- */
 ArmInverseKinematicsTest::ArmInverseKinematicsTest(RTC::Manager* manager)
-    // <rtc-template block="initializer">
-  : RTC::DataFlowComponentBase(manager)
-
-    // </rtc-template>
+: RTC::DataFlowComponentBase(manager),
+	m_qCurIn("qCur", m_qCur),
+	m_axesIn("axes", m_axes),
+	m_buttonIn("button", m_button),
+	m_qRefOut("qRef", m_qRef),
+	gain(0.1)
 {
 }
 
-/*!
- * @brief destructor
- */
+
 ArmInverseKinematicsTest::~ArmInverseKinematicsTest()
 {
 }
@@ -51,113 +35,87 @@ ArmInverseKinematicsTest::~ArmInverseKinematicsTest()
 
 RTC::ReturnCode_t ArmInverseKinematicsTest::onInitialize()
 {
-  // Registration: InPort/OutPort/Service
-  // <rtc-template block="registration">
-  // Set InPort buffers
-  
-  // Set OutPort buffer
-  
-  // Set service provider to Ports
-  
-  // Set service consumers to Ports
-  
-  // Set CORBA Service Ports
-  
-  // </rtc-template>
+	addInPort("qCur", m_qCurIn);
+	addInPort("axes", m_axesIn);
+	addInPort("button", m_buttonIn);
 
-  // <rtc-template block="bind_config">
-  // </rtc-template>
-  
-  return RTC::RTC_OK;
+	addOutPort("qRef",m_qRefOut);
+
+	arm = new Kinematics(ulink);
+	SetJointInfo(ulink);
+
+	return RTC::RTC_OK;
 }
-
-/*
-RTC::ReturnCode_t ArmInverseKinematicsTest::onFinalize()
-{
-  return RTC::RTC_OK;
-}
-*/
-
-/*
-RTC::ReturnCode_t ArmInverseKinematicsTest::onStartup(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
-
-/*
-RTC::ReturnCode_t ArmInverseKinematicsTest::onShutdown(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
-
 
 RTC::ReturnCode_t ArmInverseKinematicsTest::onActivated(RTC::UniqueId ec_id)
 {
-  return RTC::RTC_OK;
+	if(m_qCurIn.isNew()){
+		m_qCurIn.read();
+		m_qRef.data.length(m_qCur.data.length());
+	}
+	for(size_t i=0;i<JOINT_NUM;i++)
+		ulink[i+1].q = m_qCur.data[i];
+	for(size_t i=0;i<m_qCur.data.length();i++)
+		m_qRef.data[i] = m_qCur.data[i];
+	
+	arm->calcForwardKinematics(WAIST);
+	
+	RARM_Link = ulink[RARM_JOINT7];
+
+	return RTC::RTC_OK;
 }
 
 
 RTC::ReturnCode_t ArmInverseKinematicsTest::onDeactivated(RTC::UniqueId ec_id)
 {
-  return RTC::RTC_OK;
+	return RTC::RTC_OK;
 }
 
 
 RTC::ReturnCode_t ArmInverseKinematicsTest::onExecute(RTC::UniqueId ec_id)
 {
-  return RTC::RTC_OK;
+	if(m_axesIn.isNew())
+		m_axesIn.read();
+	if(m_buttonIn.isNew())
+		m_buttonIn.read();
+	if(m_qCurIn.isNew()){
+		m_qCurIn.read();
+
+		if(m_button.data[0] == 1){
+			m_qRef.data[35] += 0.001;
+			m_qRef.data[36] -= 0.001;
+		}else if(m_button.data[1] == 1){
+			m_qRef.data[35] -= 0.001;
+			m_qRef.data[36] += 0.001;
+		}
+
+		RARM_Link.p(0) += m_axes.data[1]*0.1;
+		RARM_Link.p(1) += m_axes.data[0]*0.1;
+		RARM_Link.p(2) += m_axes.data[3]*0.1;
+
+		if(arm->calcArmInverseKinematics(RARM_JOINT7, RARM_Link)){
+			for(int i=13;i<=25;i++)
+				m_qRef.data[i-1] = ulink[i].q;
+		}else{
+			cerr << "Calculation Inverse Kinematics Faild." << endl;
+		}
+	}
+	m_qRefOut.write();
+
+	return RTC::RTC_OK;
 }
-
-/*
-RTC::ReturnCode_t ArmInverseKinematicsTest::onAborting(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
-
-/*
-RTC::ReturnCode_t ArmInverseKinematicsTest::onError(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
-
-/*
-RTC::ReturnCode_t ArmInverseKinematicsTest::onReset(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
-
-/*
-RTC::ReturnCode_t ArmInverseKinematicsTest::onStateUpdate(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
-
-/*
-RTC::ReturnCode_t ArmInverseKinematicsTest::onRateChanged(RTC::UniqueId ec_id)
-{
-  return RTC::RTC_OK;
-}
-*/
-
-
 
 extern "C"
 {
- 
-  void ArmInverseKinematicsTestInit(RTC::Manager* manager)
-  {
-    coil::Properties profile(arminversekinematicstest_spec);
-    manager->registerFactory(profile,
-                             RTC::Create<ArmInverseKinematicsTest>,
-                             RTC::Delete<ArmInverseKinematicsTest>);
-  }
-  
+
+	void ArmInverseKinematicsTestInit(RTC::Manager* manager)
+	{
+		coil::Properties profile(arminversekinematicstest_spec);
+		manager->registerFactory(profile,
+				RTC::Create<ArmInverseKinematicsTest>,
+				RTC::Delete<ArmInverseKinematicsTest>);
+	}
+
 };
 
 
