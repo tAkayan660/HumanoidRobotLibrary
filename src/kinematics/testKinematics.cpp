a10c678ed4d28c29a6f80e2667c834b8d71710f5
RyuYamamoto <ron_robotics@yahoo.co.jp>
2016-12-16 16:17:19 +0900

    fixed func

--- src/kinematics/testKinematics.cpp
+++ src/kinematics/testKinematics.cpp
@@ -17,24 +17,25 @@ int main(int argc, char* argv[])
 	cout << RARM_JOINT0 << endl;
 
 	ulink[RARM_JOINT0].q = 0.0;
-	ulink[RARM_JOINT1].q = deg2rad(30);
-	ulink[RARM_JOINT2].q = deg2rad(0);
+	ulink[RARM_JOINT1].q = deg2rad(40);
+	ulink[RARM_JOINT2].q = deg2rad(-15);
 	ulink[RARM_JOINT3].q = 0.0;
 	ulink[RARM_JOINT4].q = deg2rad(-90);
-	ulink[RARM_JOINT5].q = deg2rad(0);
+	ulink[RARM_JOINT5].q = deg2rad(-15);
 	ulink[RARM_JOINT6].q = 0.0;
-	ulink[RARM_JOINT7].q = deg2rad(-30);
+	ulink[RARM_JOINT7].q = deg2rad(-40);
 
 	//Calculation Forward Kinematics
 	kine.calcForwardKinematics(WAIST);
 	Link RARM_LINK = ulink[RARM_JOINT7];
 	
-	for(int i=0;i<50;i++){
-		RARM_LINK.p(2) += 0.1;
+	for(int i=0;i<1000;i++){
+		RARM_LINK.p(2) -= 0.1;
 		//Calculation Inverse Kinematics
-		if(kine.calcArmInverseKinematics(RARM_JOINT7, RARM_LINK)){
+		cout << "iteration:" << i << endl;
+		if(kine.calcInverseKinematics(RARM_JOINT7, RARM_LINK)){
 			for(int i=13;i<=25;i++)
-				cout << rad2deg(ulink[i].q) << endl;
+				cout << ulink[i].joint_name << ":" << rad2deg(ulink[i].q) << endl;
 		}else{
 			cout << "Calculation Inverse Kinematics Faild." << endl;
 		}
